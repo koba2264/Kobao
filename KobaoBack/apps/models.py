78bdc19157e794f3af039cb67a2fcdbc3e4ed313
koba2264 <s2001k1022@gmail.com>
2025-08-21 10:42:43 +0900

    models変更

--- KobaoBack/apps/models.py
+++ KobaoBack/apps/models.py
@@ -29,11 +29,15 @@ class Teacher(db.Model):
     name = db.Column(db.String(20), nullable=False)
     hash_pass = db.Column(db.String(60), nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
+    create_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, nullable=False)
+    update_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, nullable=False)
+
+    answers = db.relationship('Answer', backref='teacher', cascade='all, delete-orphan')
 
 
 class Question(db.Model):
     __tablename__ = 'questions'
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     asked_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, nullable=False)
     ansed_flag = db.Column(db.Boolean, default=False, nullable=False)
@@ -45,9 +49,10 @@ class Question(db.Model):
 
 class Answer(db.Model):
     __tablename__ = 'answer'
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = db.Column(UUID(as_uuid=True), primary_key=True)
     content = db.Column(db.Text, nullable=False)
     answerd_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, nullable=False)
+    tea_id = db.Column(db.String(7), db.ForeignKey('teacher.id', ondelete='CASCADE'), nullable=False)
 
     qa = db.relationship('QA', backref='answer', cascade='all, delete-orphan')
     tags = db.relationship('Tag', secondary='anstag', backref='answers')
@@ -55,7 +60,7 @@ class Answer(db.Model):
 
 class QA(db.Model):
     __tablename__ = 'qa'
-    que_id = db.Column(UUID(as_uuid=True), db.ForeignKey('questions.id', ondelete='CASCADE'), primary_key=True)
+    que_id = db.Column(db.Integer, db.ForeignKey('questions.id', ondelete='CASCADE'), primary_key=True)
     ans_id = db.Column(UUID(as_uuid=True), db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
 
 
