a249dc09d32e604222372d01c34602f4e3343991
koba2264 <144329354+koba2264@users.noreply.github.com>
2025-08-29 12:59:21 +0900

    Update app.py

--- KobaoBack/apps/app.py
+++ KobaoBack/apps/app.py
@@ -19,7 +19,8 @@ def create_app(config_key):
     # あとで受け取れるサイトを絞る
     CORS(app, supports_credentials=True, origins=[ "http://localhost:8081",
         "https://3_cof8m-yuichiroito-8081.exp.direct",
-        "https://6d5lk2c-h7w4-8081.exp.direct"
+        "https://6d5lk2c-h7w4-8081.exp.direct",
+        "http://163.43.210.29"
  ])
     # jwt認証用
     jwt.init_app(app)
